5ca5e2e812741766f28465526fd2b0a289c62310
jbostoen <->
2020-12-15 17:36:23 +0100

    Version 2020-12-15 17:36:23
    * remove languages script: match 'dictionary' entries

--- remove_languages.sh
+++ remove_languages.sh
@@ -1,11 +1,10 @@
 # Beware: files will be deleted if you uncomment the line with "Deleted" in it.
 # Adjust path of iTop directory
-# Adjust the line with (en|nl) to languages you want to keep. Example (en|fr|nl)
+# Adjust the line with (en|fr|nl) to languages you want to keep. Example (en|fr|nl)
 iTopDir="/var/www/html/itop/web"
-find $iTopDir -type f -regex ".*/.+\.dict\..+\.php" -exec bash -c '
+find $iTopDir -type f -regex ".*/.+\.\(dict\|dictionary\)\..+\.php" -exec bash -c '
         for item do
                 short_name=${item##*/}
-
                 [[ $short_name =~ ^[a-z_]+\.(dict|dictionary)\..* && ! $short_name =~ ^(en|fr|nl) ]] && echo "[X] $short_name" || echo "[ ] $short_name"
                 # [[ $short_name =~ ^[a-z_]+\.(dict|dictionary)\..* && ! $short_name =~ ^(en|fr|nl) ]] && rm $item &&  echo "    Deleted"
         done
